d98d996be97187aa0c172fd560a92a5df50dd4ea
Hank <hank15hank@yahoo.com.tw>
2019-01-09 22:07:22 +0800

    added user Manager

--- system/drink/admin.py
+++ system/drink/admin.py
@@ -6,13 +6,18 @@ class IngredientAdmin(admin.ModelAdmin):
     list_display = ('ingredient_name','amount','is_processed')
 class CustomerAdmin(admin.ModelAdmin):
     list_display = ('customer_phone','name','gender','points','create_time','latest_order_time')
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id','date','total_price','customer')
+class Order_has_productAdmin(admin.ModelAdmin):
+    list_display = ('order','product','amount')
+    
 # Register your models here.
 admin.site.register(Customer,CustomerAdmin)
 admin.site.register(Supplier)
 admin.site.register(Ingredient,IngredientAdmin)
 admin.site.register(Product)         
-admin.site.register(Order)         
-admin.site.register(Order_has_product)         
+admin.site.register(Order,OrderAdmin)         
+admin.site.register(Order_has_product,Order_has_productAdmin)         
 admin.site.register(Product_made_by_ingredient)         
 admin.site.register(Ingredient_offerd_by_supplier)   
 admin.site.register(ROP_Parameters)              
